d2a0c1139f66facf7256384ac2ff1725be6044c6
shutaozhenzhen <3462232313@qq.com>
2018-05-14 23:39:19 +0800

    Create unicode.js

---
+++ unicode.js
@@ -0,0 +1,114 @@
+list = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "<="]
+function init() {
+  $ui.render({
+    views: [{
+      type: "input",
+      props: {
+        id: "text-input",
+        text: "0000"
+      },
+      layout: function (make, view) {
+        make.left.top.inset(10);
+        make.height.equalTo(32);
+        make.right.inset(100);
+      },
+      events: {
+        ready: function (sender) {
+          sender.focus()
+        },
+        returned: function (sender) {
+          sender.blur()
+        },
+        didEndEditing: function (sender) {
+          if (sender.text.split("").length > 4) {
+            var t = [], l = sender.text.split("");
+            for (var k = 0; k < 4; k++) {
+              t[k] = l[k];
+            }
+            $("text-input").text = t.join("");
+          }
+        }
+      }
+    },
+    {
+      type: "button",
+      props: {
+        title: "send"
+      },
+      layout: function (make, view) {
+        make.top.right.inset(10);
+        make.height.equalTo(32);
+        make.left.equalTo($("text-input").right).offset(10)
+      },
+      events: {
+        tapped: function (sender) {
+          if ($app.env == $env.keyboard) {
+            var
+              txt = $("text-input").text;
+            $keyboard.insert(String(eval('"\\u' + txt + '"')));
+          }else{
+            var
+            txt = $("text-input").text;
+          $clipboard.text= String(eval('"\\u' + txt + '"'));
+          }
+        }
+
+      }
+    },
+    {
+      type: "matrix",
+      props: {
+        columns: 4,
+        itemHeight: 50,
+        spacing: 25,
+        template: {
+          views: [
+            {
+              type: "label",
+              props: {
+                id: "label",
+                align: $align.center,
+                font: $font(36)
+              },
+              layout: $layout.fill
+            }
+          ]
+        },
+        data: list.map(function (item) {
+          return {
+            label: {
+              text: item
+            }
+          }
+        })
+      },
+      layout: function (make, view) {
+        make.top.equalTo($("text-input").bottom)
+        make.left.bottom.right.equalTo(0)
+      },
+      events: {
+        didSelect: function (sender, indexPath) {
+          var t = list[indexPath.row];
+          if (t == "<=") {
+            var k = $("text-input").text.split("")
+            k.pop();
+            $("text-input").text = "0" + k.join("");
+          } else {
+            var k = $("text-input").text.split("")
+            if (k.length == 4) {
+              k.shift();
+              $("text-input").text = k.join("") + t;
+            } else {
+              $("text-input").text = k.join("") + t;
+            }
+
+          }
+          var txt = String(eval('"\\u' + $("text-input").text + '"'))
+          $("button").title = "send:" + txt
+        }
+      }
+    }
+    ]
+  })
+}
+init();
